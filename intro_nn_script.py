4c9cc6f7054d38ee243f87b2a01caff889fa17a0
Ian Delbridge <idelbrid@gmail.com>
2016-07-31 14:49:22 -0400

    added biases. Slightly better accuracy as expected

--- intro_nn_script.py
+++ intro_nn_script.py
@@ -18,8 +18,8 @@ def sigmoid(x):
 
 # @nb.jit(nb.typeof((ex_np_arr, ex_np_arr))(ar_type, ar_type, ar_type, ar_type, nb.typeof(6000)),
 #         nopython=True)
-@nb.jit
-def train(X, y, w0, w1, iterations, batch_size=500, learn_rate=3.0):
+# @nb.jit
+def train(X, y, w0, w1, b1, b2, iterations, batch_size=500, learn_rate=3.0):
 
     bs = batch_size
     # Xpartition = [X[bs*i:bs*(i+1)] for i in range(int(np.ceil(len(X)/bs)))]
@@ -41,21 +41,23 @@ def train(X, y, w0, w1, iterations, batch_size=500, learn_rate=3.0):
         for i in range(int(np.ceil(len(y) / bs))):
             a0 = X[(i*bs):((i+1)*bs)]
             t = y[(i*bs):((i+1)*bs)]
-            a1 = sigmoid(np.dot(a0, w0.T))
-            a2 = sigmoid(np.dot(a1, w1.T))
+            a1 = sigmoid(np.dot(a0, w0.T) + b1)
+            a2 = sigmoid(np.dot(a1, w1.T) + b2)
 
             delta2 = (t - a2) * (a2 * (1 - a2))
             delta1 = np.dot(delta2, w1) * (a1 * (1 - a1))
 
             w1 += learn_rate * np.dot(delta2.T, a1) / bs
             w0 += learn_rate * np.dot(delta1.T, a0) / bs
+            b2 += learn_rate * delta2.sum(axis=0) / bs
+            b1 += learn_rate * delta1.sum(axis=0) / bs
 
-    return (w0, w1)
+    return (w0, w1, b1, b2)
 
 
-@nb.jit(ar_type(ar_type, ar_type, ar_type), nopython=True)
-def feed_forward(X, w0, w1):
-    return sigmoid(np.dot(sigmoid(np.dot(X, w0.T)), w1.T))  # need to add biases
+@nb.jit(ar_type(ar_type, ar_type, ar_type, ar_type, ar_type), nopython=True)
+def feed_forward(X, w0, w1, b1, b2):
+    return sigmoid(np.dot(sigmoid(np.dot(X, w0.T) + b1), w1.T) + b2)  # need to add biases
 
 
 def to_distr_repr(y):
@@ -66,18 +68,18 @@ def to_distr_repr(y):
     return toreturn
 
 
-def save_model(w0, w1, filename='./mlp_model.pkl'):
+def save_model(w0, w1, b1, b2, filename='./mlp_model.pkl'):
 
     with open(filename, 'wb') as f:
         pickler = pickle.Pickler(f, protocol=pickle.HIGHEST_PROTOCOL)
-        pickler.dump((w0, w1))
+        pickler.dump((w0, w1, b1, b2))
 
 
 def load_model(filename='./mlp_model.pkl'):
 
     with open(filename, 'rb') as f:
-        (w0, w1) = pickle.load(f)
-    return w0, w1
+        (w0, w1, b1, b2) = pickle.load(f)
+    return w0, w1, b1, b2
 
 
 if __name__ == '__main__':
@@ -125,19 +127,19 @@ if __name__ == '__main__':
 
     myw0 = np.random.randn(hidden_layer_size, X.shape[1])
     myw1 = np.random.randn(10, hidden_layer_size)
-    # myb1 = np.random.randn(1, hidden_layer_size)
-    # myb2 = np.random.randn(1, 10)
+    myb1 = np.random.randn(1, hidden_layer_size)
+    myb2 = np.random.randn(1, 10)
 
-    # w0, w1 = train(X, y, myw0, myw1, 150, 15)
-    # save_model(w0, w1)
-    w0, w1 = load_model()
+    # w0, w1, b1, b2 = train(X, y, myw0, myw1, myb1, myb2, 150, 15)
+    # save_model(w0, w1, b1, b2)
+    w0, w1, b1, b2 = load_model()
 
-    y_pred = feed_forward(X * 1.0, w0, w1)  # should be good since this is training data
+    y_pred = feed_forward(X * 1.0, w0, w1, b1, b2)  # should be good since this is training data
     y_pred_num = np.argmax(y_pred, axis=1)
     accuracy = np.count_nonzero(y_pred_num == y_numbers) / len(y)
     print("Accuracy on train set", accuracy)
 
-    y_test_pred = feed_forward(test_X * 1.0, w0, w1)
+    y_test_pred = feed_forward(test_X * 1.0, w0, w1, b1, b2)
     y_test_pred_num = np.argmax(y_test_pred, axis=1)
     test_accuracy = np.count_nonzero(y_test_pred_num == test_y_numbers) / len(test_y)
     print("Accuracy on test set", test_accuracy)
@@ -148,7 +150,7 @@ if __name__ == '__main__':
     sample_pred_test_y_num = y_test_pred_num[:20]
 
     for x, y, pred in zip(sample_test_x, sample_test_y_num, sample_pred_test_y_num):
-        plt.pcolor(np.reshape(x, (28, 28)), cmap='Greys')
+        plt.pcolor(np.reshape(x, (28, 28))[::-1, ::1], cmap='Greys')
         plt.title('Actual {}, pred {}'.format(y, pred))
         plt.show()
 
